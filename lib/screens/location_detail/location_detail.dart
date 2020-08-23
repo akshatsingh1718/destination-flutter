53740ca3eeacdf70e4f515b22a9e4343917a698f
akshat.singh1718 <akshat.singh1718@gmail.com>
2020-08-23 20:44:09 +0530

    added ImageBanner in destination screen

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import './image_banner.dart';
+import './text_section.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hellow World"),
+      ),
+      body: Column(
+        /*
+        For column MainAxisAlignment is from top to bottom
+        and for row left to right
+        */
+        mainAxisAlignment: MainAxisAlignment.start,
+        /**
+         * For column CrossAxisAlignment is opposite of MainAxisAlignment
+         * so it is left to right and same for Row as well.
+         */
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ImageBanner("assets/images/dest.jpg"),
+          TextSection(Colors.red),
+          TextSection(Colors.green),
+          TextSection(Colors.amber),
+        ],
+      ),
+    );
+  }
+}
